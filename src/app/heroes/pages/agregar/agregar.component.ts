938013aec6060ca526fdffb718e5aeffa897742f
josepereza <josepereza66@gmail.com>
2021-02-18 17:58:30 +0100

    cambio en buscar.html

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class AgregarComponent implements OnInit {
-
+  heroe:Heroe={
+   alter_ego :'',
+   superhero: '',
+   characters: '',
+   publisher:Publisher.DCComics,
+   first_appearance:'',
+   alt_img: ''
+  }
+ publisher=[
+{
+  id:"DC Comics",
+  desc:'Dc - Comics'
+},
+{
+  id:"Marvel Comics",
+  desc:'Marvel Comics'
+},
+{
+  id:"DC Comics",
+  desc:'Dc - Comics'
+},
+ ]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 
 import {MatSidenavModule} from '@angular/material/sidenav';
@@ -10,20 +9,21 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [],
   exports: [
     MatAutocompleteModule,
     MatInputModule,
-    MatFormFieldModule,
+   
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
     MatListModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatSelectModule
   ]
 })
 export class MaterialModule { }
